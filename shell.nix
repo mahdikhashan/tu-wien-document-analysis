85ab7e39bfd4e8bd5c30a56dd85d739821233899
mahdikhashan <mahdikhashan1@gmail.com>
2025-03-24 13:31:05 +0100

    init

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    pkgs.python311
+    pkgs.python3Packages.virtualenv
+    pkgs.python311Packages.notebook
+    pkgs.python311Packages.jupyterlab
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.opencv4
+  ];
+
+  shellHook = ''
+    python --version
+    if [ ! -d "venv" ]; then
+       virtualenv venv
+    fi
+    source venv/bin/activate
+  '';
+}
